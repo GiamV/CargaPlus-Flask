17d20ed4edd50d0bd066c2d977c8d12a13582d8c
gustavo <u21220756@utp.edu.pe>
2024-12-04 12:50:21 -0500

    Chatbot

--- bd.py
+++ bd.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '1234'
+    MYSQL_PASSWORD = ''
     MYSQL_DB = 'SistemaAsistencia'
 
 config = {
